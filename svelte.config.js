d1dd0462bb7d2cce45d9f8e95ec824487e6623d3
Björn Christensson <bjorn.christensson@prorenata.se>
2025-08-10 18:42:07 +0200

    install the missing static adapter and set the fallback to index.html

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,14 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: true,
-			strict: true,
-			prerender: {
-				default: true
-			}
+			fallback: 'index.html'
 		})
 	}
 };
